433319c956e13623b2ea64b7823c1459245e0360
missnerea <ela.amish@gmail.com>
2018-03-16 19:10:39 +0300

    add neighbourhood model

--- hood/watch/models.py
+++ hood/watch/models.py
@@ -3,3 +3,9 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Neighbourhood(admin.ModelAdmin):
+    name = model.CharField(max_length=60)
+    location = model.CharField(max_length=60)
+    occupants_count = model.CharField(max_length=60)
+    user = models.ForeignKey(User)
