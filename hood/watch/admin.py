54c2fc2f4ab38c462398c7e332ae27e8742ccb35
missnerea <ela.amish@gmail.com>
2018-03-19 18:13:47 +0300

    add neighbourhood admin class

--- hood/watch/admin.py
+++ hood/watch/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Neighbourhood
+from .models import Neighbourhood , Business
 
+class NeighbourhoodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'location']
 
+    def related_business(self, obj):
+        return obj.businessid.name
+    related_business.short_description = 'Business'
 
 # Register your models here.
-admin.site.register(Neighbourhood)
+admin.site.register(Neighbourhood,NeighbourhoodAdmin)
--- hood/watch/models.py
+++ hood/watch/models.py
@@ -6,15 +6,22 @@ from django.contrib import admin
 # Create your models here.
 
 class Neighbourhood(models.Model):
-    neighbourhoodid = models.CharField(max_length=255, primary_key=True, db_column='neighbourhoodID')
+    neighbourhoodid = models.CharField(max_length=255, primary_key=True, db_column='neighbourhoodID', blank=True)
     name = models.CharField(max_length=60)
     location = models.CharField(max_length=60)
     occupants_count = models.CharField(max_length=60)
     user = models.ForeignKey(User)
 
+    def __unicode__(self):
+        return self.name
+
 
 class Business(models.Model):
+    businessid = models.CharField(max_length=255, primary_key=True, db_column='businessID', blank=True)
     name = models.CharField(max_length=30)
     emailaddress = models.CharField(max_length=60)
     user = models.ForeignKey(User)
     neighbourhoodid = models.ForeignKey(Neighbourhood,null=True,db_column='neighbourhoodID',blank=True)
+
+    def __unicode__(self):
+        return self.name
