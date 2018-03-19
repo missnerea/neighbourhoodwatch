00d55510de4b8b3114bea6365c5a997b450f3b4b
missnerea <ela.amish@gmail.com>
2018-03-19 17:46:34 +0300

    add fields to business model

--- hood/watch/admin.py
+++ hood/watch/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from . models import Neighbourhood
+from .models import Neighbourhood
+
+
 
 # Register your models here.
 admin.site.register(Neighbourhood)
--- hood/watch/models.py
+++ hood/watch/models.py
@@ -1,11 +1,20 @@
 from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib import admin
 
 # Create your models here.
 
-class Neighbourhood(admin.ModelAdmin):
-    name = model.CharField(max_length=60)
-    location = model.CharField(max_length=60)
-    occupants_count = model.CharField(max_length=60)
+class Neighbourhood(models.Model):
+    neighbourhoodid = models.CharField(max_length=255, primary_key=True, db_column='neighbourhoodID')
+    name = models.CharField(max_length=60)
+    location = models.CharField(max_length=60)
+    occupants_count = models.CharField(max_length=60)
     user = models.ForeignKey(User)
+
+
+class Business(models.Model):
+    name = models.CharField(max_length=30)
+    email address = models.CharField(max_length=60)
+    user = models.ForeignKey(User)
+    neighbourhoodid = models.ForeignKey(Neighbourhood,null=True,db_column='neighbourhoodID',blank=True)
