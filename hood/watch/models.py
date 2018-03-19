c19cb863ddb2410de548e209264ffc4b839a7ed6
missnerea <ela.amish@gmail.com>
2018-03-19 17:47:28 +0300

    fix business field error

--- hood/watch/models.py
+++ hood/watch/models.py
@@ -15,6 +15,6 @@ class Neighbourhood(models.Model):
 
 class Business(models.Model):
     name = models.CharField(max_length=30)
-    email address = models.CharField(max_length=60)
+    emailaddress = models.CharField(max_length=60)
     user = models.ForeignKey(User)
     neighbourhoodid = models.ForeignKey(Neighbourhood,null=True,db_column='neighbourhoodID',blank=True)
